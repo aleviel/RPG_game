527a6eaaec85f112c5b017c50ebaa7d220ca9eaa
Alexey R <alex666alex@mail.ru>
2021-07-04 22:16:52 +0300

    added canvas, person and simple move logic for person

--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
 import './index.scss';
+import Sensei from './assets/Male-3-Walk.png';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const SETTINGS = {
+    spriteW: 48,
+    spriteH: 48,
+    cycle: 0,
+    shots: 3,
+    speed: 10,
+    CanvasWidth: canvas.width,
+    CanvasHeight: canvas.height,
+    pY: 0,
+    pX: 0,
+};
+
+const KEYS = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false,
+};
+
+const PersonImg = document.createElement('img');
+PersonImg.src = Sensei;
+
+PersonImg.addEventListener('load', () => {
+    document.querySelector('h3').remove();
+    SETTINGS.pY = SETTINGS.CanvasHeight / 2 - SETTINGS.spriteH / 2;
+    SETTINGS.pX = SETTINGS.CanvasWidth / 2 - SETTINGS.spriteW / 2;
+    setInterval(() => {
+        // eslint-disable-next-line no-unused-expressions
+        KEYS.ArrowDown &&
+            SETTINGS.pY < SETTINGS.CanvasHeight - SETTINGS.spriteH &&
+            (SETTINGS.pY += SETTINGS.speed) &&
+            (SETTINGS.cycle = (SETTINGS.cycle + 1) % SETTINGS.shots);
+        // eslint-disable-next-line no-unused-expressions
+        KEYS.ArrowUp &&
+            SETTINGS.pY > 0 &&
+            (SETTINGS.pY -= SETTINGS.speed) &&
+            (SETTINGS.cycle = (SETTINGS.cycle + 1) % SETTINGS.shots);
+        // eslint-disable-next-line no-unused-expressions
+        KEYS.ArrowLeft &&
+            SETTINGS.pX > 0 &&
+            (SETTINGS.pX -= SETTINGS.speed) &&
+            (SETTINGS.cycle = (SETTINGS.cycle + 1) % SETTINGS.shots);
+        // eslint-disable-next-line no-unused-expressions
+        KEYS.ArrowRight &&
+            SETTINGS.pX < SETTINGS.CanvasWidth - SETTINGS.spriteW &&
+            (SETTINGS.pX += SETTINGS.speed) &&
+            (SETTINGS.cycle = (SETTINGS.cycle + 1) % SETTINGS.shots);
+
+        ctx.clearRect(0, 0, 600, 600);
+        ctx.drawImage(
+            PersonImg,
+            SETTINGS.cycle * SETTINGS.spriteW,
+            0,
+            SETTINGS.spriteW,
+            SETTINGS.spriteH,
+            SETTINGS.pX,
+            SETTINGS.pY,
+            SETTINGS.spriteW,
+            SETTINGS.spriteH,
+        );
+    }, 120);
+});
+
+function startRun(event) {
+    event.preventDefault();
+    KEYS[event.key] = true;
+}
+
+function stopRun(event) {
+    event.preventDefault();
+    KEYS[event.key] = false;
+}
+
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
